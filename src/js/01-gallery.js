824158075a5f6a0c571e1cb4d800993754e3e269
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2022-10-09 15:24:05 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,24 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-// Opisany w dokumentacji
-// Dodatkowy import stylów
+console.log(galleryItems)
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+const newGallery = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+    <div><a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}"
+    alt="${description}"
+    title="${description}" />
+    </a></div>
+    `;
+  })
+  .join('');
+
+const galleryPictures = document.querySelector('div.gallery');
+
+galleryPictures.insertAdjacentHTML('beforeend', newGallery);
+//new SimpleLightbox('.gallery a', { /* options */ });
+const galleryLightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
