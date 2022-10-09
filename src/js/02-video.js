18cf97d830861b152d943739baf61e6761a4ca5b
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2022-10-09 20:05:16 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import { throttle } from 'lodash';
+
+const iframePlayer = new Vimeo.Player(document.querySelector('#vimeo-player'));
+
+const startTime = parseFloat(localStorage.getItem('videoplayer-current-time')) || 0;
+
+iframePlayer.setCurrentTime(startTime);
+
+function onPlaySeconds(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+  console.log(
+    'Local saved time: ' + localStorage.getItem('videoplayer-current-time')
+  );
+}
+
+iframePlayer.on('timeupdate', throttle(onPlaySeconds, 1000));
 
