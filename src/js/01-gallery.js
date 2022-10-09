38c998ab8453fa7b67da8db24639d4f6f359db0e
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2022-10-09 15:00:42 +0200

    Create 01-gallery.js

---
+++ src/js/01-gallery.js
@@ -0,0 +1,8 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+// Opisany w dokumentacji
+// Dodatkowy import stylów
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
